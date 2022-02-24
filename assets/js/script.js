b0f8727ce4281ffd5cb7bdcbcc8d873156e8cf70
JESSE <dtrsjesse121@gmail.com>
2022-02-24 18:16:55 -0500

    add fetch requests to get weather data and display it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,54 @@
+//Variables
+
 var button = document.querySelector('.submit')
 var inputValue = document.querySelector('.input-value')
-var mainCity = document.querySelector('main-city')
-var mainTemp = document.querySelector('main-temp')
-var mainWind = document.querySelector('main-wind')
-var mainHum = document.querySelector('main-hum')
-var mainUv = document.querySelector('main-uv')
+var mainCity = document.querySelector('.main-city')
+var mainTemp = document.querySelector('.main-temp')
+var mainWind = document.querySelector('.main-wind')
+var mainHum = document.querySelector('.main-hum')
+var mainUv = document.querySelector('.main-uv')
 
-
+//Button Click Functionality
 
 button.addEventListener('click', function(){
 
+//Fetch to find coordinates and city name
+
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=7930f77cca8484bf43bfc2ebd049bdcd')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        var cityLat = data['coord']['lat'];
+        var cityLon = data['coord']['lon'];
+        var cityValue = data['name'];
+  
+//Use name and coordinates we just aquired to fetch weather data
 
-    .catch(err => alert('city name not valid!'))
-    
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+cityLat+'&lon='+cityLon+'&units=imperial&appid=7930f77cca8484bf43bfc2ebd049bdcd')
+    .then(response => response.json())
+    .then(data => {
+        var cityTemp = data['current']['temp'];
+        var cityWind = data['current']['wind_speed'];
+        var cityHum = data['current']['humidity'];
+        var cityUv = data['current']['uvi'];
+        
+//Display values ro weather display
+
+        mainCity.innerHTML = cityValue;
+        mainTemp.innerHTML = 'Temp: ' + cityTemp + '°F';
+        mainWind.innerHTML = 'Wind: ' + cityWind;
+        mainHum.innerHTML = 'Humidity: ' + cityHum;
+        mainUv.innerHTML = 'UV Index: ' + cityUv;
+        
+        //console.log('name', cityValue, 'temp', cityTemp, 'wind', cityWind, 'hum', cityHum, 'uv',cityUv)
+    })
 })
+})
+
     
+
+
+
+
+
+
+
